f93f4b4df1d86f29f24de96f9c321f36f6d9816b
Oliver Oxenham <oliver@cardcity.co>
2019-01-08 21:42:30 +0400

    - updated webpack config to build blocks and components separately

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,10 +1,42 @@
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-  entry: './main.js',
+const isDirectory = source => fs.statSync(source).isDirectory();
+
+const getAllEntries = (dir) => {
+  const entries = {};
+  fs.readdirSync(`./src/${dir}`).map(name => {
+    if (isDirectory(`./src/${dir}/${name}`)) {
+      entries[name] = `./src/${dir}/${name}/${name}`;
+    }
+  });
+  if (dir === 'blocks') {
+    entries.main = './main';
+  }
+  console.log(entries);
+  return entries;
+};
+
+module.exports = [{
+  entry: getAllEntries('blocks'),
   externals: {
-    react: 'react',
-    'react-dom': 'react-dom'
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    },
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components'
+    }
   },
   module: {
     rules: [
@@ -17,14 +49,54 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        use: ['@svgr/webpack', 'url-loader'],
+        use: ['@svgr/webpack', 'url-loader']
       }
     ]
   },
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'main.js',
-    library: 'codecraftor-template',
+    path: path.resolve(__dirname, '../dist/blocks'),
+    filename: '[name].js',
     libraryTarget: 'umd'
   }
-};
+}, {
+  entry: getAllEntries('components'),
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    },
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader']
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist/components'),
+    filename: '[name].js',
+    libraryTarget: 'umd'
+  }
+}];
---
+++ src/blocks/Test/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  font-size: 20px;
+  color: hotpink;
+  border: 1px solid black;
+`
+
+export default () => <StyledDiv>test</StyledDiv>
